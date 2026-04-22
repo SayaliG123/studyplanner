be9880dfee1efbbac70bfae4eaab4b58767957c7
SayaliG123 <sayalighadge0902@gmail.com>
2026-04-22 20:18:51 +0530

    Added new feature / updated code

--- src/com/studyplanner/Task.java
+++ src/com/studyplanner/Task.java
@@ -2,9 +2,20 @@ package com.studyplanner;
 
 public class Task {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	String subject;
+	int difficulty_level;
+	int days_left;
+	
+	public Task (String subject,int difficulty_level,int days_left )
+	{
+		this.subject=subject;
+		this.difficulty_level=difficulty_level;
+		this.days_left=days_left;
 	}
+	public String toString()
+	{
+		return " subject: "+subject +"| difficulty level: " + difficulty_level+"|days left: "+days_left;
+	}
+	
 
 }
