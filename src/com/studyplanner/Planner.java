d5c7214b789d4467179a3361285ae50ece89982a
SayaliG123 <sayalighadge0902@gmail.com>
2026-04-23 18:26:10 +0530

    Added new feature

--- src/com/studyplanner/Planner.java
+++ src/com/studyplanner/Planner.java
@@ -1,10 +1,35 @@
 package com.studyplanner;
 
+import java.util.ArrayList;
+import java.util.*;
+
 public class Planner {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public void sorttask(ArrayList<Task>task)
+	{
+		Collections.sort(task,(a,b)->
+		{
+			int p1= a.difficulty_level*2+(10-a.days_left);
+			int p2= b.difficulty_level*2+(10-b.days_left);
+			return p2-p1;
+					
+		});
+	}
+	public void generateplan (ArrayList<Task>task)
+	{
+		System.out.println(" --- Study Plan ---");
+		
+		for(Task t:task)
+		{
+			int hours= t.difficulty_level*2;
+			
+			if(t.days_left<3)//this is panic mode more hours for study are required
+			{
+				hours=+2;
+			}
+			System.out.println(t.subject+ " -> "+ hours+" Hours");
+			
+		}
 	}
 
 }
